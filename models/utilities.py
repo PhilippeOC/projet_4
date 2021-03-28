e21b65a1b2fcfc33b79561f809dbd222265656b0
PhilippeOC <74734623+PhilippeOC@users.noreply.github.com>
2021-03-28 22:15:42 +0200

    Delete models directory

--- models/utilities.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def check_name(name: str):
-    """ retourne False si name contient des caractères de ponctuation,
-    des chiffres ou des caractères spéciaux"""
-    if re.search(r"^[A-Za-z '-éèàîïùç]{2,}$", name):
-        return False
-
